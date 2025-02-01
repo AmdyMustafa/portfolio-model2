a9054fe29c8953913e90c62d45fe12c0cb4d404b
tapha221 <amdydaba@gmail.com>
2025-02-01 19:29:36 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './index.css';
+import Header from "./components/Header.jsx";
+/*import About from './components/About.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';*/
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/*<About />
+      <Portfolio />
+      <Contact />
+      <Footer />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+// Header.jsx
+import "../css/Header.css";
+
+function Header() {
+  return (
+        <nav className="barre_entete" id="mainNav">
+                <a className="titre" href="#page-top">PORTFILIO</a>
+                {/*<button>
+                    Menu
+                    <i className="fas fa-bars"></i>
+                </button>*/}
+                
+                    <ul id="menu">
+                        <li ><a href="#portfolio">Portfolio</a></li>
+                        <li ><a href="#about">About</a></li>
+                        <li ><a href="#contact">Contact</a></li>
+                    </ul>
+                
+        </nav>
+  );
+}
+
+
+export default Header;
