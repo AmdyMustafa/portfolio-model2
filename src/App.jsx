30b709f92c90f5f4ee4c86f9aa275f01e4acc317
tapha221 <amdydaba@gmail.com>
2025-02-02 00:57:19 +0100

    intersection-observer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import './index.css';
-import Header from "./components/Header.jsx";
-import Masthead from "./components/masthead.jsx";
-import Projets from "./components/Projets.jsx";
-/*import About from './components/About.jsx';
-import Contact from './components/Contact.jsx';
-import Footer from './components/Footer.jsx';*/
+import React, { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import Header from './components/Header';
+import MastHead from './components/mastHead';
+import Projets from './components/Projets';
 
 function App() {
+  const [activeSection, setActiveSection] = useState('section_head');
+
+  const [headRef, headInView] = useInView({ threshold: 0.6 });
+  const [projetsRef, projetsInView] = useInView({ threshold: 0.6 });
+  const [contactRef, contactInView] = useInView({ threshold: 0.6 });
+
+  // Utilisez useEffect pour gérer les mises à jour d'état
+  useEffect(() => {
+    if (headInView) setActiveSection('section_head');
+    if (projetsInView) setActiveSection('section_projets');
+    if (contactInView) setActiveSection('contact');
+  }, [headInView, projetsInView, contactInView]); // Déclenche useEffect uniquement lorsque ces valeurs changent
+
   return (
     <div>
-      <Header />
-      <Masthead />
-      <Projets />
-      {/*<About />
-      
-      <Contact />
-      <Footer />*/}
+      <Header activeSection={activeSection} />
+      <section id="section_head" ref={headRef}>
+        <MastHead />
+      </section>
+      <section id="section_projets" ref={projetsRef}>
+        <Projets />
+      </section>
+      <section id="contact" ref={contactRef}>
+        {/* Ajoutez votre composant Contact ici */}
+      </section>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,29 @@
 // Header.jsx
 import "../css/Header.css";
 
-function Header() {
-  return (
+function Header({ activeSection }) {
+    return (
         <nav className="barre_entete" id="mainNav">
-                <a className="titre" href="#page-top">PORTFILIO</a>
-                {/*<button>
+            <a className="titre" href="#page-top">PORTFILIO</a>
+            {/*<button>
                     Menu
                     <i className="fas fa-bars"></i>
                 </button>*/}
-                
-                    <ul id="menu">
-                        <li ><a href="#portfolio">Projets</a></li>
-                        <li ><a href="#about">About</a></li>
-                        <li ><a href="#contact">Contact</a></li>
-                    </ul>
-                
+
+            <ul id="menu">
+                <li className={activeSection === 'section_head' ? 'active' : ''}>
+                    <a href="#section_head" >Projects</a>
+                </li>
+                <li className={activeSection === 'section_projets' ? 'active' : ''}>
+                    <a href="#section_projets">About</a>
+                </li>
+                <li className={activeSection === 'section_contacts' ? 'active' : ''}>
+                    <a href="#section_contact">Contacts</a>
+                </li>
+            </ul>
+
         </nav>
-  );
+    );
 }
 
 
