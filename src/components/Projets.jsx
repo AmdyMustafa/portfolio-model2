5fc4986954ef7ef316180eb9fdbf3ad1b601dcf2
tapha221 <amdydaba@gamail.com>
2025-02-03 10:23:00 +0100

    title section projet et line+étoile

--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -3,9 +3,16 @@ import "../css/Projets.css";
 function Projets() {
   return (
     <section className="section_projet">
+      <h2 >PORTFOLIO</h2>
+        {/* Icon Divider*/}
+        <div className="lines-star">
+          <div className="line"></div>
+          <div><i className="fas fa-star"></i></div>
+          <div className="line"></div>
+        </div>
     </section>
   );
 }
 
-
 export default Projets;
+
