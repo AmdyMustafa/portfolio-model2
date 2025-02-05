04bc2f4dcf45956dc35706e45cb689c22143a183
tapha221 <amdydaba@gmail.com>
2025-02-05 21:57:32 +0100

    Recevoir les message envoyé dans contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,43 +1,54 @@
 import "../css/Contact.css";
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm } from "@formspree/react";
+import { useEffect, useRef } from "react";
 
 function Contacts() {
   const [state, handleSubmit] = useForm("xzzdvpnr");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
+  const formRef = useRef(null); // Référence pour le formulaire
+
+  useEffect(() => {
+    if (state.succeeded) {
+      window.confirm("✅ Merci! Votre message est envoyé.");
+      formRef.current.reset(); // Effacer le formulaire
+    }
+  }, [state.succeeded]);
+  
+
   return (
-    <section className="section_contacts" onSubmit={handleSubmit}>
-      <h2 >Contacts</h2>
-      {/* Icon Divider*/}
+    <section className="section_contacts">
+      <h2>Contacts</h2>
+      {/* Icon Divider */}
       <div className="lines-star">
         <div className="line"></div>
         <div><i className="fas fa-star"></i></div>
         <div className="line"></div>
       </div>
-      <form id="contactForm">
-          <div className="form-elem">
-            <input className="input" id="name" type="text" placeholder=" " required />
-            <label for="name">Full name</label>
-            </div>
 
-          <div className="form-elem">
-            <input className="input"  id="email" type="email" placeholder=" " required />
-            <label for="email">Email address</label>
-            </div>
+      <form id="contactForm" ref={formRef} onSubmit={handleSubmit}>
+        <div className="form-elem">
+          <input className="input" id="name" name="name" type="text" placeholder=" " required />
+          <label htmlFor="name">Full name</label>
+        </div>
 
-          <div className="form-elem">
-            <input className="input"  id="phone" type="tel" placeholder=" " required />
-            <label for="phone">Phone number</label>
-            </div>
+        <div className="form-elem">
+          <input className="input" id="email" name="email" type="email" placeholder=" " required />
+          <label htmlFor="email">Email address</label>
+        </div>
 
-          <div className="form-elem">
-            <textarea className="input" id="message" required placeholder=" " ></textarea>
-            <label for="message">Message</label>
-            </div>
+        <div className="form-elem">
+          <input className="input" id="phone" name="phone" type="tel" placeholder=" " required />
+          <label htmlFor="phone">Phone number</label>
+        </div>
 
-          <button className="button_send" type="submit" disabled={state.submitting}>Send</button>
-        </form>
+        <div className="form-elem">
+          <textarea className="input" id="message" name="message" required placeholder=" "></textarea>
+          <label htmlFor="message">Message</label>
+        </div>
+
+        <button className="button_send" type="submit" disabled={state.submitting}>
+          Send
+        </button>
+      </form>
     </section>
   );
 }
