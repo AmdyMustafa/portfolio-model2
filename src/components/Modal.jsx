940ae1cf928ffaeb517c0961178e769f5b3f0135
tapha221 <amdydaba@gmail.com>
2025-02-16 18:04:12 +0100

    modal fenetre

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,23 @@
 import "../css/Modal.css";
 
-const Modal = ({ isOpen, onClose, titre, src ,desciption}) => {
+const Modal = ({ isOpen, onClose, titre, src, desciption }) => {
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className="close-btn1" onClick={onClose}>
-                    ✕
-                </button>
-                <h2 >{titre}</h2>
-                {/* Icon Divider*/}
-                <div className="lines-star">
-                    <div className="line"></div>
-                    <div><i className="fas fa-star"></i></div>
-                    <div className="line"></div>
+                <div className="partie_gauche">
+                    <button className="close-btn1" onClick={onClose}>
+                        ✕
+                    </button>
+                    <h3>{titre}</h3>
+                    <p className="description">{desciption}</p>
+                    <button className="close-btn2" onClick={onClose}>
+                        ✕ Fermer
+                    </button>
+
                 </div>
                 <img className="img_modal" src={src} alt="" />
-                <p className="description">{desciption}</p>
-                <button className="close-btn2" onClick={onClose}>
-                    ✕ Fermer
-                </button>
             </div>
         </div>
     );
